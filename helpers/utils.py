e874cc99638a4a94f4e3e53fdb39ae627bba450d
Eduardo Barreto Brito <eduardobarreto97@gmail.com>
2023-02-26 13:42:34 -0300

    Executing iris with Kfold

--- helpers/utils.py
+++ helpers/utils.py
@@ -1,8 +1,8 @@
 import numpy as np
 from sklearn.multiclass import OneVsOneClassifier, OneVsRestClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import normalize
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, classification_report
 from sklearn import datasets
 
 import sys
@@ -10,11 +10,16 @@ import os
 sys.path.append(os.path.abspath('../models'))
 from models.icq_scikit_estimator import IcqClassifier
 
+K_FOLDS=10
+
 def executeIris(random_seed = 1, 
                 classifier_function=None, 
                 sigma_q_weights=[1,1,1,0], 
                 one_vs_classifier=OneVsRestClassifier, 
-                max_iter=3000):
+                max_iter=3000,
+                plot_graphs_in_classifier=False,
+                print_each_fold_metric=True,
+                print_avg_metric=True):
     """
         Executes ICQ classifier against Iris dataset (loaded from scikit.datasets) using classifier_function as classifier (either ../helpers/icq_methods.create_and_execute_classifier or ../helpers/icq_methods.create_and_execute_classifier_new_approach.)
     """
@@ -24,28 +29,80 @@ def executeIris(random_seed = 1,
     y = iris.target
 
     # split training set and test set
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state = random_seed, stratify = y)
+    skf = StratifiedKFold(n_splits=K_FOLDS)
 
-    normalized_X_train = normalize(X_train)
-    normalized_X_test  = normalize(X_test)
+    scores = []
+    f1scores = []
+    for i, (train_index, test_index) in enumerate(skf.split(X, y)):
+        X_train = X[train_index]
+        X_test = X[test_index]
+
+        y_train = y[train_index]
+        y_test = y[test_index]
+
+        normalized_X_train = normalize(X_train)
+        normalized_X_test  = normalize(X_test)
+
+        clf = one_vs_classifier(
+            IcqClassifier(
+                classifier_function=classifier_function, 
+                sigma_q_weights=sigma_q_weights,
+                max_iter=max_iter,
+                accuracy_succ=1.0,
+                plot_graphs_and_metrics=plot_graphs_in_classifier,
+                random_seed=random_seed)).fit(normalized_X_train, y_train)
+
+        score = clf.score(normalized_X_test, y_test)
+        f1score = f1_score(clf.predict(normalized_X_test), y_test, average='macro')
+
+        scores.append(score)
+        f1scores.append(f1score)
+
+        if print_each_fold_metric:
+            y_pred = clf.predict(X_test)
+            print("K-Fold #" + str(i) + ":")
+            print(classification_report(y_test, y_pred))
+            print("-------------------------------------------------------------------------------------------------------------------")
     
-    clf = one_vs_classifier(
-        IcqClassifier(
-            classifier_function=classifier_function, 
-            sigma_q_weights = sigma_q_weights,
-            max_iter=max_iter,
-            rate_succ=1.0)).fit(normalized_X_train, y_train)
+    if print_avg_metric:
+        print("AVG: Scores =", np.mean(scores), "F1-Scores =", np.mean(f1scores))
+    return scores, f1scores
 
-    score = clf.score(normalized_X_test, y_test)
-    f1score = f1_score(clf.predict(normalized_X_test), y_test, average='macro')
+def executeIrisOneVsRest(random_seed=1, 
+                        classifier_function=None, 
+                        sigma_q_weights=[1,1,1,0], 
+                        max_iter=3000,
+                        print_each_fold_metric=True,
+                        print_avg_metric=True):
+    """
+        Uses executeIris with sklearn.multiclass.OneVsRestClassifier
+    """
+    return executeIris(random_seed,
+                       classifier_function,
+                       sigma_q_weights,
+                       OneVsRestClassifier,
+                       max_iter,
+                       print_each_fold_metric=print_each_fold_metric,
+                       plot_graphs_in_classifier=False,
+                       print_avg_metric=print_avg_metric)
 
-    return score, f1score
-
-def executeIrisOneVsRest(random_seed = 1, classifier_function=None, sigma_q_weights=[1,1,1,0], max_iter=3000):
-    return executeIris(random_seed, classifier_function, sigma_q_weights, OneVsRestClassifier, max_iter)
-
-def executeIrisOneVsOne(random_seed = 1, classifier_function=None, sigma_q_weights=[1,1,1,0], max_iter=3000):
-    return executeIris(random_seed, classifier_function, sigma_q_weights, OneVsOneClassifier, max_iter)
+def executeIrisOneVsOne(random_seed=1, 
+                        classifier_function=None, 
+                        sigma_q_weights=[1,1,1,0], 
+                        max_iter=3000,
+                        print_each_fold_metric=True,
+                        print_avg_metric=True):
+    """
+        Uses executeIris with sklearn.multiclass.OneVsOneClassifier
+    """
+    return executeIris(random_seed,
+                       classifier_function,
+                       sigma_q_weights,
+                       OneVsOneClassifier,
+                       max_iter,
+                       print_each_fold_metric=print_each_fold_metric,
+                       plot_graphs_in_classifier=False,
+                       print_avg_metric=print_avg_metric)
 
 def print_metrics(scores, f1scores):
     print("Scores:", scores)
